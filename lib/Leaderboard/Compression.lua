136c524bd01ef294bc83344b443b145e41f93637
Kumarion <121711454+Kumarion@users.noreply.github.com>
2023-11-05 00:16:30 -0400

    Quick fixes

--- lib/Leaderboard/Compression.lua
+++ lib/Leaderboard/Compression.lua
@@ -1,19 +1,20 @@
 local Compression = {};
+local LOGARITHMIC_BASE = 1.0000001;
 
-local function CompressedNumber(hugeNumber: number): number
-	return hugeNumber ~= 0 and math.floor(math.log(hugeNumber) / math.log(1.0000001)) or 0;
+local function CompressedNumber(num: number): number
+	return (num ~= 0 and math.floor(math.log(num) / math.log(LOGARITHMIC_BASE)) or 0);
 end
 
-local function DecompressedNumber(stored: number): number
-	return math.floor(stored ~= 0 and (1.0000001 ^ stored) or 0)
+local function DecompressedNumber(num: number): number
+	return (math.floor(num ~= 0 and (math.pow(LOGARITHMIC_BASE, num)) or 0));
 end
 
 function Compression.Compress(num: number): number
 	return CompressedNumber(num);
 end
 
-function Compression.Decompress(stored: number): number
-	return DecompressedNumber(stored);
+function Compression.Decompress(num: number): number
+	return DecompressedNumber(num);
 end
 
 return Compression;
